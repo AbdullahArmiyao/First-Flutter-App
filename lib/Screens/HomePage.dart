7733bbd39bf42d117c083ad6987ac62b6fcb4dfe
Abdullah Armiyao <53265844+AbdullahArmiyao@users.noreply.github.com>
2025-03-03 10:44:04 +0000

    Added function to retrieve username and profile picture

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -27,12 +27,69 @@ class _homepage extends State<Homepage> {
     }
   }
 
+  Future<String?> getUsername() async {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      return user.displayName;
+    } else {
+      return null;
+    }
+  }
+
+  Future<String?> getProfilePicture() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return user.photoURL; // URL of the profile picture
+    } else {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.transparent,
         title: Text("Home"),
+        actions: [
+          FutureBuilder<String?>(
+            future: getUsername(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Text('Error fetching username');
+              } else if (snapshot.hasData) {
+                return Text(snapshot.data ?? 'No username');
+              } else {
+                return Text('No username');
+              }
+            },
+          ),
+          FutureBuilder<String?>(
+              future: getProfilePicture(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Icon(Icons.error, size: 50);
+                } else if (snapshot.hasData && snapshot.data != null) {
+                  // If there is a profile picture URL
+                  return CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(snapshot.data!),
+                  );
+                } else {
+                  // If no profile picture is available
+                  return CircleAvatar(
+                    radius: 50,
+                    child: Icon(Icons.person, size: 50),
+                  );
+                }
+              }
+            ),
+        ],
       ),
       body: SingleChildScrollView(
         child: SafeArea(
@@ -40,6 +97,7 @@ class _homepage extends State<Homepage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              SizedBox(height: 40,),
               Text("Welcome To The Homepage"),
             ],
           ),
@@ -50,7 +108,8 @@ class _homepage extends State<Homepage> {
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add"),
           BottomNavigationBarItem(
-            icon: IconButton(onPressed: _handleLogout, icon: Icon(Icons.logout)),
+            icon:
+                IconButton(onPressed: _handleLogout, icon: Icon(Icons.logout)),
             label: "Logout",
           ),
         ],
